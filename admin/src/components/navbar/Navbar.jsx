44736e72b874b51b90292785eb7a324567195595
Mohitsaini2028 <mohitsaini00720@gmail.com>
2022-07-07 12:34:04 +0530

    design update

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -8,9 +8,11 @@ import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutline
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
   const { dispatch } = useContext(DarkModeContext);
+  const {user} = useContext(AuthContext);
 
   return (
     <div className="navbar">
@@ -46,7 +48,8 @@ const Navbar = () => {
           </div>
           <div className="item">
             <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              // src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src={`${user.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"}`  }
               alt=""
               className="avatar"
             />
